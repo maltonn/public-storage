08c51307b9d5310b93ab077785e82826ff2fda4c
moscwa <ymatsubayashi09@outlook.jp>
2019-10-14 22:31:06 +0900

    s/3

---
+++ s/2/3/contents.js
@@ -0,0 +1,15 @@
+﻿
+ques_list_dict = {
+  "crown":['asteroid', 'exploration', 'unprecedented', 'fields', 'ambitious', 'date from', 'achieve', 'next to impossible', 'attempt', 'immediately', 'observe', 'leak', 'window of opportunity', 'capsule', 'withstand', 'farewell', 'day in and day out', 'somehow', 'burn up', 'anbitious', 'farther', 'conclude', 'late mother', 'going through', 'correspondence', 'correspond with', 'profits', 'naval', 'principally', 'principal', 'principle', 'adventures', 'respected', 'respectavle', 'respective', 'respectful', 'featured', 'direct', 'apart', 'boarding school', 'destination', 'attend', 'station', 'reunion', 'attract', 'granted', 'fascinate', 'take for granted', 'make all the difference'],
+}
+
+ans_list_dict = {
+  "crown":['小惑星', '探求', '前例のない', '分野', '熱意のある', '～にさかのぼる', '達成する', 'ほとんど不可能な', '試み', 'すぐに', '述べる', '漏れる', 'わずかなチャンス', 'カプセル', '耐える', '別れの挨拶', '来る日も来る日も', '何とかして', '燃え尽きる', 'すばらしい', 'より遠くへ', '結論付ける', '亡き母', '目を通す', '手紙', '一致する・文通する', '利益', '海軍の', '主に', '重要な', '原理', '冒険', '立派な', 'ちゃんとした', 'それぞれの', '敬意に満ちた', '取り上げられる', '手紙を書く', '離れて', '寄宿学校', '目的地', '（学校に）通う', '配属する', '再開のつどい', '魅惑する', '許可する', '魅了する', '主要に思う', '重要である'],
+}
+
+font_size_dict = {
+  "sample": "1.4rem",
+}
+selection_dict = {
+  "crown":'英1'
+}
